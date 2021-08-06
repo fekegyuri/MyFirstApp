20b6660290375716d67041d43c7645b6af7c9b3e
fekegyuri <88340912+fekegyuri@users.noreply.github.com>
2021-08-07 00:17:47 +0200

    9. nap volt 2

--- vscode-practice/kilencedik-nap/scripts.js
+++ vscode-practice/kilencedik-nap/scripts.js
@@ -52,3 +52,22 @@ $('button').click(MegEgyLi) */
 $('#minusz').click(() => {
     $("li:last-of-type").remove()
 })
+
+//Ciklusok
+
+for (let i = 0; i < 100; i = i + 1) {
+    $('ul').append(`<li>${i + 1}. Nem csalok többet.</li>`)    
+}
+
+let szinek = ['orange', 'magenta', 'red', 'brown', 'green', 'blue', 'teal']
+
+function egySzinDoboz(szin) {
+    $('.kontener').append('<div class="doboz"></div>')
+    $('.kontener div:last-of-type').css('background-color', szin)
+    
+}
+
+egySzinDoboz('lime')
+egySzinDoboz('pink')
+
+szinek.forEach(egySzinDoboz)
